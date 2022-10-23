d84b084a6819a7745866a6dcfe4ea054c994b174
DamirStipa <stipa.damir@hotmail.com>
2022-10-23 13:55:26 +0200

    Rework FavoriteButton compose design. Does not receive or change any data yet, is just clickable and changes design on click.

--- app/src/main/java/agency/five/codebase/android/movieapp/ui/component/FavoriteButton.kt
+++ app/src/main/java/agency/five/codebase/android/movieapp/ui/component/FavoriteButton.kt
@@ -1,38 +1,38 @@
 package agency.five.codebase.android.movieapp.ui.component
 
 import agency.five.codebase.android.movieapp.R
-import agency.five.codebase.android.movieapp.mock.MoviesMock
-import agency.five.codebase.android.movieapp.model.MovieDetails
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
+//This image is only clickable and does not receive any data atm
+
 @Composable
 fun FavoriteButton(
     modifier: Modifier = Modifier,
-    contentDescription: String = "Favorite button",
-    movieDetails: MovieDetails
+    contentDescription: String = "Favorite button"
 ){
-
+    val fav = remember { mutableStateOf(false) }
     Image(
-        painter = painterResource(id = if(movieDetails.movie.isFavorite) R.drawable.ic_favorite_selected else R.drawable.ic_favorite),
+        painter = painterResource(id = if(fav.value) R.drawable.ic_favorite_selected else R.drawable.ic_favorite),
         contentDescription = contentDescription,
         modifier = modifier
             .width(28.dp)
             .width(28.dp)
             .clickable {
-                movieDetails.movie.isFavorite = !movieDetails.movie.isFavorite
+                fav.value = !fav.value
             })
 }
 
 @Preview(showBackground = true)
 @Composable
 private fun FavoriteButtonPreview() {
-    val movieDetails = MoviesMock.getMovieDetails()
-    FavoriteButton(movieDetails = movieDetails)
+    FavoriteButton()
 }
